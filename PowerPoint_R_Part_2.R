951b3b7b9cba8517410cdb92276e14e37ce1ebb5
vinnief <vincy@sapo.cv>
2016-06-19 11:58:39 +0800

    finally works again. some titles in italian somehow.

--- PowerPoint_R_Part_2.R
+++ PowerPoint_R_Part_2.R
@@ -9,10 +9,15 @@ library(rvest)
 clint_url <- "http://www.imdb.com/name/nm0000142/"
 
 #Set your local path here
-local_path <- "./Clint Eastwood"
-local_file <- paste0(local_path,"/Clint Eastwood - IMDb.html")
-test_page <- read_html(local_file)
-clint_page <- read_html(clint_url)
+local_folder<- "Clint Eastwood"
+local_path <- paste(".", local_folder,sep="/")
+# Create the img sub-directory
+if (!file.exists(local_folder)) dir.create(local_folder)
+#pre-download the clint_url page manually into the local folder to test if reading works (even without internet)
+local_file <- paste(local_path, "Clint Eastwood - IMDb.html",sep="/")
+
+test_page <- read_html(local_file, encoding="UTF-8")
+clint_page <- read_html(clint_url, encoding="UTF-8")
 
 #film_selector <- ".filmo-row"
 film_selector <- "#filmo-head-actor+ .filmo-category-section .filmo-row"
@@ -23,23 +28,21 @@ filmography <- filmography[-grep("TV Movie|TV Series",html_text(filmography))]
 
 # Create the films data frame
 films <- NULL
-films$year <- filmography %>% html_nodes("span") %>% html_text() %>% str_trim()
-films$title <- filmography %>% html_nodes("b a") %>% html_text()
+films$year <- filmography %>% html_nodes("span") %>% html_text() %>% str_trim()%>% str_extract("\\d+") #eliminate rubbish characters in year
+films$title <- filmography %>% html_nodes("b a") %>% html_text() # some titles are in italian. why?
 films$url <- paste0("http://www.imdb.com",filmography %>% html_nodes("b a") %>% html_attr("href"))
 films <- as.data.frame(films,stringsAsFactors=FALSE)
 
-# Create the img sub-directory
-if (!file.exists("img")) dir.create("img")
 #Create an index so the dataframe can be sorted back.
 films$index <- sprintf("%02d",seq_along(1:length(films$year)))
 
-films$img_file <- paste0("img/img",films$index,".jpg")
+films$img_file <- paste0(local_folder,"/img",films$index,".jpg")
 
 # Extract the character name and add to dataframe
-# check exact xpath with Selectorgadget chrome extention
+# check exact xpath with Selectorgadget chrome extention #IMDB xpaths can change over time.
 get_character <- function(film,filmography) {
     i <- as.integer(film$index)
-    character_name <- filmography[i] %>% html_nodes(xpath=".//a") %>% html_text()
+    character_name <- filmography[[i]] %>% html_nodes("br+ a") %>% html_text()
     if (length(character_name)==0) {
         character_name <- filmography[[i]] %>%
                             html_nodes(xpath="text()[preceding-sibling::br]") %>%
@@ -50,7 +53,7 @@ get_character <- function(film,filmography) {
     return(character_name)
 }
 
-films$character_name <- daply(films,.(index),get_character,filmography) #had error: not same dimensions. because of xpath in get_character ".//a[2]
+films$character_name <- daply(films,.(index),get_character,filmography) #had error: not same dimensions. because of xpath in get_character ".//a[2] then eror
 
 # Loop through the films and download the poster image into the "img" subdirectory.
 # If the poster is not found, flag the file name with 0.
@@ -58,30 +61,30 @@ for (i in 1:nrow(films)) {
     img_node <- read_html(films$url[i]) %>%
                 html_nodes(xpath='//*[(@id = "title-overview-widget")]//img') #//td[@id="img_primary"]//img_primary
     if (length(img_node)==0) {
-        films$img_file[i] <- "img/img00.jpg"
-        cat(i," : img file NOT FOUND: ",films$img_file[i],"\n")
+        films$img_file[i] <- "img00.png"
+        cat(i,"th : img file NOT FOUND: replacing by ",films$img_file[i],"\n")
     }
     else {
         img_link <- html_attr(img_node,"src")
-        cat(i," :",films$img_file[i]," : ",img_link,"\n")
+        cat(i," :",films$img_file[i]," : ", img_link,"\n")
         download.file(img_link,films$img_file[i],method="internal",mode="wb")
     }
 }
 
 # Check which of the files were not found and download them manually
-films$title[which(films$img_file=="img/img00.jpg")]
-
-# These images didn't exist in version 1. so Downloaded manually
-# now commented out
+films$title[which(films$img_file=="img00.png")]
+#none!
+# These images didn't exist in version 1. so then they had to be Downloaded manually
+# now commented outas no problem?
 #films[55,"img_file"] <- "img/img55.jpg"
 #films[54,"img_file"] <- "img/img54.jpg"
 #films[52,"img_file"] <- "img/img52.jpg"
-
 # Correct this title (appears with strange characters because of my locale)
-films[40,"title"] <- "Kelly's Heroes"
+#films[40,"title"] <- "Kelly's Heroes"
 
 # Save the data frame
-write.table(films,file="eastwood_films.csv",append=FALSE,quote=TRUE,sep="\t",row.names=FALSE)
+write.table(films,file=paste(local_path,"eastwood_films.tsv", sep="/"),append=FALSE,quote=TRUE,sep="\t",row.names=FALSE)
+write.table(films,file=paste(local_path,"eastwood_films.csv", sep="/"),append=FALSE,quote=TRUE,sep=",",row.names=FALSE)
 
 #------------------------------------- Films dataframe done -------------------------------------#
 
@@ -89,7 +92,7 @@ write.table(films,file="eastwood_films.csv",append=FALSE,quote=TRUE,sep="\t",row
 # =====================================Create a dataframe for box office earnings data ========#
 ##  Get box office earnings data for the films
 clint_box_office_url <- "http://www.boxofficemojo.com/people/chart/?id=clinteastwood.htm"
-bo_page <- html(clint_box_office_url)
+box_office_page <- read_html(clint_box_office_url)
 # Extract tables. The fourth table is the one we want, with adjusted box office returns
 bo <- box_office_page %>% html_table(header=TRUE,fill=TRUE) %>% (function(x) {x[[4]]})
 
@@ -99,7 +102,7 @@ bo$adjusted_gross <- as.numeric(gsub("[\\$\\,]","",bo$adjusted_gross))
 bo$unadjusted_gross <- as.numeric(gsub("[\\$\\,]","",bo$unadjusted_gross))
 bo$release_date <- strptime(bo$release_date,"%m/%d/%y")
 bo$release_date[32] <- strptime("1975-06-15",format("%Y-%m-%d"))
-bo$release_date <- correct_date(box_office$release_date)
+#bo$release_date <- correct_date(box_office$release_date)
 
 # Create a key for joining dataframes using the film title
 bo$key <- tolower(gsub("[^[:alnum:]]", "", bo$title))
@@ -110,4 +113,6 @@ bo$key[34] <- "therookie"
 box_office <- left_join(select(bo,bo_rank,studio,adjusted_gross,key),select(films,year,title,index,key),by="key")
 
 # Save the box_office data frame.
-write.table(box_office,file="eastwood_box_office.csv",append=FALSE,quote=TRUE,sep="\t",row.names=FALSE)
+write.table(box_office,file=paste(local_path,"eastwood_box_office.tsv", sep="/"),append=FALSE,quote=TRUE,sep="\t",row.names=FALSE)
+write.table(box_office,file=paste(local_path,"eastwood_box_office.csv", sep="/"),append=FALSE,quote=TRUE,sep=",",row.names=FALSE)
+
